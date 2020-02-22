50dd6476321ee6486f6ebe8cc01696f0938b2ce9
League Student <league@iMac-24.attlocal.net>
2020-02-21 17:00:08 -0800

    Module 1

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -1,7 +1,8 @@
 package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
-	//1. Create a private int array. Don't initialize it.
+	// 1. Create a private int array. Don't initialize it.
+	private int[] nums;
 
 	public DynamicIntegerArray() {
 		// 2. Initialize the int array to have 0 elements.
