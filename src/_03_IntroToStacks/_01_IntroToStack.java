9b8f5b582ee802b7501aab9cf65da30dcd46b61e
League Student <league@iMac-11.attlocal.net>
2020-03-09 16:56:22 -0700

    IntroToStack, TextUndoRedo(IP)

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,36 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> doubles = new Stack();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		Random r = new Random();
+		for (int i = 0; i < 100; i++) {
+			double num = r.nextInt(101);
+			doubles.push(num);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		String fakenum1 = JOptionPane.showInputDialog("Enter a number between 0 and 100 (inclusive)");
+		String fakenum2 = JOptionPane.showInputDialog("Enter a number between 0 and 100 (inclusive)");
+		int num1 = Integer.parseInt(fakenum1);
+		int num2 = Integer.parseInt(fakenum2);
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
+		if(num1<num2) {
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+		} else if(num1==num2) {
+			System.out.println("AN ERROR HAS OCCURED");
+		}
+			
 		
+		for (int i = 0; i < doubles.size(); i++) {
+			double temp = doubles.pop();
+			if(temp>num2 && temp<num1) {
+				System.out.println(temp);
+			}
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,11 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.*;
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +18,50 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	public static void main(String[] args) {
+		_02_TextUndoRedo t = new _02_TextUndoRedo();
+		t.setup();
+	}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	String doc = "";
+	Stack<Character> deleted = new Stack();
 	
+	void setup(){
+		frame.addKeyListener(this);
+		panel.add(label);
+		frame.add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode()==KeyEvent.VK_DELETE) {
+			String text = label.getText();
+			label.setText(text.substring(0, text.length()-1));
+			System.out.println("hello this works");
+		}
+		char letter = e.getKeyChar();
+		doc = doc+letter;
+		label.setText(doc);
+		frame.pack();
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
