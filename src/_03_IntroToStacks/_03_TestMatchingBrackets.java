ab146cc826709c3f4f657da0002545d6bb084201
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2020-03-23 16:58:48 -0700

    TestMatchingBrackets, HangMang (IP)

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -21,27 +21,28 @@ public class _03_TestMatchingBrackets {
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
 	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		int rightCount = 0;
-		int leftCount = 0;
-		Stack<String> stack = new Stack();
-		stack.push(b);
-		String current = stack.pop();
-		if (current.charAt(0) == '}') {
-			return false;
-		} else {
-			for (int i = 0; i < current.length(); i++) {
-				if (current.charAt(i) == '{') {
-					leftCount++;
+		Stack<Character> stack = new Stack();
+		String current = b;
+		for (int i = 0; i < current.length(); i++) {
+			char temp = current.charAt(i);
+			if(temp==('{')) {
+				stack.push('{');
+			} else if(temp==('}')) {
+				if(stack.size()>0) {
+					stack.pop();
 				} else {
-					rightCount++;
+					return false;
 				}
 			}
-			if (rightCount == leftCount) {
-				return true;
-			} else {
-				return false;
-			}
 		}
+		if(stack.size()==0) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		
+	
 
 	}
 
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,70 @@
 package _04_HangMan;
 
-public class HangMan{
-	
-	
-	
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+
+public class HangMan implements KeyListener {
+	public static void main(String[] args) {
+		HangMan h = new HangMan();
+		h.start();
+		h.setup();
+	}
+
+	Stack<String> stack = new Stack<String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	int numOfWords;
+	int lives = 6;
+	String currentWord;
+	String blanks = lives+" lives				";
+	void start() {
+		String yeet = JOptionPane.showInputDialog("How many rounds of hangman would you like to play?");
+		numOfWords = Integer.parseInt(yeet);
+		for (int i = 0; i < numOfWords; i++) {
+			String word = Utilities.readRandomLineFromFile("dictionary.txt");
+			stack.push(word);
+		}
+		currentWord = stack.pop();
+		blanks = blanks+"\n";
+		for (int i = 0; i < currentWord.length(); i++) {
+			blanks = blanks+"_ ";
+		}
+	}
+
+	void setup() {
+		label.setText(blanks);
+		panel.add(label);
+		frame.add(panel);
+		frame.addKeyListener(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.pack();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char letter = e.getKeyChar();
+		if(currentWord.contains(letter+"")) {
+			
+		} else {
+			lives--;
+		}
+	}
 }
