8b0a824ae6399939e5f0117f23010520f8ad9649
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2020-03-30 16:57:57 -0700

    Hangman(IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -20,7 +20,11 @@ public class HangMan implements KeyListener {
 	int numOfWords;
 	int lives = 6;
 	String currentWord;
-	String blanks = lives+" lives				";
+	String livesword = lives + " lives				";
+	String blanks = "";
+	String shown;
+	String[] options = {"yes", "no"};	
+
 	void start() {
 		String yeet = JOptionPane.showInputDialog("How many rounds of hangman would you like to play?");
 		numOfWords = Integer.parseInt(yeet);
@@ -29,14 +33,14 @@ public class HangMan implements KeyListener {
 			stack.push(word);
 		}
 		currentWord = stack.pop();
-		blanks = blanks+"\n";
+
 		for (int i = 0; i < currentWord.length(); i++) {
-			blanks = blanks+"_ ";
+			blanks = blanks + "_";
 		}
 	}
 
 	void setup() {
-		label.setText(blanks);
+		label.setText(livesword + blanks);
 		panel.add(label);
 		frame.add(panel);
 		frame.addKeyListener(this);
@@ -54,17 +58,49 @@ public class HangMan implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		int index = 0;
+		int counter = 0;
+		char letter = e.getKeyChar();
+		StringBuilder sb = new StringBuilder(blanks);
+		if (currentWord.contains(letter + "")) {
+			while (currentWord.indexOf(letter, index) > -1) {
+				index = currentWord.indexOf(letter, index);
+				sb.setCharAt(index, letter);
+				index++;
+				blanks = sb.toString();
+				counter++;
+			}
+			label.setText(livesword + blanks);
+			frame.pack();
+			
+			
+		} else {
+			lives--;
+			label.setText(livesword + blanks);
+			int x= 1;
+			frame.pack();
+			if(lives==0){
+				livesword = lives + " lives				";
+				label.setText("GAME OVER");
+				frame.pack();
+				x = JOptionPane.showOptionDialog(null, "Would you like to play again?",
+                "Click a button",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+				System.out.println(x);
+			}
+			if(x==0) {
+				start();
+				lives = 6;
+				setup();
+				
+			}
+			
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		char letter = e.getKeyChar();
-		if(currentWord.contains(letter+"")) {
-			
-		} else {
-			lives--;
-		}
+
 	}
 }
